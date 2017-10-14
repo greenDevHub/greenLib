f8c5a108138b1936868a6411b6a818403a4c6852
EGoesche <e.goesche@gmail.com>
2017-10-14 17:54:19 +0200

    Neue Excel-Export Funktion
    (Funktioniert fast)

--- Bibo Verwaltung/ExcelExport.cs
+++ Bibo Verwaltung/ExcelExport.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Data;
+using System.IO;
 
 namespace Bibo_Verwaltung
 {
@@ -12,6 +15,32 @@ namespace Bibo_Verwaltung
         #region ExcelExport
         public void ToExcel(DataGridView dGV, string filename, string tabellenname)
         {
+            /*
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            var dt = new DataTable();
+            foreach (DataGridViewColumn column in dGV.Columns)
+            {
+                if (column.Visible)
+                {
+                    dt.Columns.Add();
+                }
+            }
+
+            object[] cellValues = new object[dGV.Columns.Count];
+            foreach (DataGridViewRow row in dGV.Rows)
+            {
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    cellValues[i] = row.Cells[i].Value;
+                }
+                dt.Rows.Add(cellValues);
+            }
+            DataSet ds = new DataSet();
+            ds.Tables.Add(dt);
+            ds.WriteXml(path);
+            */
+
             Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
             Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
             Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
@@ -41,8 +70,9 @@ namespace Bibo_Verwaltung
                 workbook.SaveAs(SpeichernDialog.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
 
             }
-            app.Quit();
+            app.Quit(); 
         }
+
         #endregion
     }
 }
