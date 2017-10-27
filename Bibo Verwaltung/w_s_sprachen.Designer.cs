b52ef3251c9b66b81d0b5d018612b411ea3f611a
RobJeh <robert-jehring@t-online.de>
2017-10-27 20:15:27 +0200

    Verbesserung der Sprachenform

--- Bibo Verwaltung/w_s_sprachen.Designer.cs
+++ Bibo Verwaltung/w_s_sprachen.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.gv_Sprachen = new System.Windows.Forms.DataGridView();
+            this.bt_Uebernehmen = new System.Windows.Forms.Button();
+            this.tb_Suchen = new System.Windows.Forms.TextBox();
+            this.lb_Sprache = new System.Windows.Forms.Label();
+            this.bt_Suchen = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.gv_Sprachen)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,23 +41,67 @@
             this.gv_Sprachen.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.gv_Sprachen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gv_Sprachen.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.gv_Sprachen.Location = new System.Drawing.Point(13, 13);
-            this.gv_Sprachen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gv_Sprachen.Location = new System.Drawing.Point(10, 12);
             this.gv_Sprachen.MultiSelect = false;
             this.gv_Sprachen.Name = "gv_Sprachen";
             this.gv_Sprachen.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.gv_Sprachen.Size = new System.Drawing.Size(256, 527);
+            this.gv_Sprachen.Size = new System.Drawing.Size(192, 396);
             this.gv_Sprachen.TabIndex = 50;
+            this.gv_Sprachen.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.gv_Sprachen_CellBeginEdit);
+            this.gv_Sprachen.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.gv_Sprachen_UserDeletedRow);
             this.gv_Sprachen.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView_KeyPress);
+            // 
+            // bt_Uebernehmen
+            // 
+            this.bt_Uebernehmen.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bt_Uebernehmen.Location = new System.Drawing.Point(10, 414);
+            this.bt_Uebernehmen.Name = "bt_Uebernehmen";
+            this.bt_Uebernehmen.Size = new System.Drawing.Size(111, 23);
+            this.bt_Uebernehmen.TabIndex = 51;
+            this.bt_Uebernehmen.Text = "Übernehmen";
+            this.bt_Uebernehmen.UseVisualStyleBackColor = true;
+            // 
+            // tb_Suchen
+            // 
+            this.tb_Suchen.Enabled = false;
+            this.tb_Suchen.Location = new System.Drawing.Point(60, 12);
+            this.tb_Suchen.Name = "tb_Suchen";
+            this.tb_Suchen.Size = new System.Drawing.Size(142, 20);
+            this.tb_Suchen.TabIndex = 52;
+            this.tb_Suchen.Visible = false;
+            this.tb_Suchen.TextChanged += new System.EventHandler(this.tb_Suchen_TextChanged);
+            // 
+            // lb_Sprache
+            // 
+            this.lb_Sprache.AutoSize = true;
+            this.lb_Sprache.Location = new System.Drawing.Point(7, 15);
+            this.lb_Sprache.Name = "lb_Sprache";
+            this.lb_Sprache.Size = new System.Drawing.Size(47, 13);
+            this.lb_Sprache.TabIndex = 53;
+            this.lb_Sprache.Text = "Suchen:";
+            this.lb_Sprache.Visible = false;
+            // 
+            // bt_Suchen
+            // 
+            this.bt_Suchen.Location = new System.Drawing.Point(127, 414);
+            this.bt_Suchen.Name = "bt_Suchen";
+            this.bt_Suchen.Size = new System.Drawing.Size(75, 23);
+            this.bt_Suchen.TabIndex = 54;
+            this.bt_Suchen.Text = "Suchen";
+            this.bt_Suchen.UseVisualStyleBackColor = true;
+            this.bt_Suchen.Click += new System.EventHandler(this.bt_Suchen_Click);
             // 
             // w_s_sprachen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 553);
+            this.ClientSize = new System.Drawing.Size(214, 446);
+            this.Controls.Add(this.bt_Suchen);
+            this.Controls.Add(this.lb_Sprache);
+            this.Controls.Add(this.tb_Suchen);
+            this.Controls.Add(this.bt_Uebernehmen);
             this.Controls.Add(this.gv_Sprachen);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "w_s_sprachen";
@@ -62,10 +110,15 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.w_s_sprachen_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.gv_Sprachen)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.DataGridView gv_Sprachen;
+        private System.Windows.Forms.Button bt_Uebernehmen;
+        private System.Windows.Forms.TextBox tb_Suchen;
+        private System.Windows.Forms.Label lb_Sprache;
+        private System.Windows.Forms.Button bt_Suchen;
     }
 }
--- Bibo Verwaltung/w_s_sprachen.cs
+++ Bibo Verwaltung/w_s_sprachen.cs
@@ -19,42 +19,88 @@ namespace Bibo_Verwaltung
             sprache.FillGrid(ref gv_Sprachen);
         }
 
+        #region globale Variablen
         Sprache sprache = new Sprache();
+        bool suchmodus = false;
+        bool aenderungungen = false;
+        #endregion
 
+        #region Schliessen-Aktion
         private void w_s_sprachen_FormClosing(object sender, FormClosingEventArgs e)
         {
-            try
+            if (aenderungungen == true)
             {
-                sprache.SaveGrid(ref gv_Sprachen);
-            }
-            catch
-            {
-                MessageBox.Show("Die Änderungen konnten nicht gespeichert werden!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                DialogResult dr = MessageBox.Show("Sollen die Änderungen gespeichert werden?", "Warnung", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (dr == DialogResult.Yes)
+                {
+                    try
+                    {
+                        sprache.SaveGrid(ref gv_Sprachen);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Die Änderungen konnten nicht gespeichert werden!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else if (dr == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
             }
         }
+        #endregion
 
+        #region Zeile löschen mit Ruecktaste
         private void dataGridView_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == (char)Keys.Back)
             {
-                try { 
-                    var selectedCell = gv_Sprachen.SelectedCells[0];
-                    gv_Sprachen.Rows.Remove(gv_Sprachen.CurrentRow);
-                }
-                catch
-                {
-                    if (gv_Sprachen.CurrentRow.Selected == true)
-                    {
-                        MessageBox.Show("Diese Zeile kann nicht entfernt werden!", "Achtung",
-                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Markieren Sie eine Zeile!", "Achtung",
-                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    }
-                }
+                    SendKeys.Send("{DELETE}");
             }
+        }
+        #endregion
+
+        private void bt_Suchen_Click(object sender, EventArgs e)
+        {
+            if (suchmodus == false)
+            {
+                suchmodus = true;
+                bt_Suchen.Text = "Suche AUS";               
+                gv_Sprachen.Size = new Size(192, 370);
+                gv_Sprachen.Location = new Point(10, 38);
+                lb_Sprache.Visible = true;
+                tb_Suchen.Visible = true;
+                tb_Suchen.Enabled = true;
+                tb_Suchen.Focus();
+            }
+            else
+            {
+                suchmodus = false;
+                bt_Suchen.Text = "Suche AN";
+                lb_Sprache.Visible = false;
+                tb_Suchen.Visible = false;
+                tb_Suchen.Enabled = false;
+                tb_Suchen.Text = "";
+                gv_Sprachen.Size = new Size(192, 396);
+                gv_Sprachen.Location = new Point(10, 12);
+            }
+        }
+
+        #region Aenderungen an GridView erkennen
+        private void gv_Sprachen_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            aenderungungen = true;
+        }
+
+        private void gv_Sprachen_UserDeletedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            aenderungungen = true;
+        }
+        #endregion
+
+        private void tb_Suchen_TextChanged(object sender, EventArgs e)
+        {
+            (gv_Sprachen.DataSource as DataTable).DefaultView.RowFilter = string.Format("sprach_name LIKE '{0}%'", tb_Suchen.Text);
         }
     }
 }
